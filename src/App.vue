32547a6a42ef4e417a6aaee98d3b849cdfcfe846
Andrei Zuica <zuica.andrei@gmail.com>
2021-06-20 10:14:56 +0300

    Added add view

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <img alt="Vue logo" width="256" height="256" src="./assets/logo.png">
+  <div>
+    <h1>Birth Certificates</h1>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Add</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">View</a></li>
+    </ul>
+    <Add/>
+  </div>
+</template>
+
+<script>
+import Add from './components/Add.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Add
+  }
+}
+</script>
+
+<style lang="scss">
+@import "node_modules/spectre.css/src/spectre";
+@import "node_modules/spectre.css/src/forms";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Add.vue
@@ -0,0 +1,70 @@
+<template>
+  <form class="form-horizontal p-centered">
+      <div class="form-group">
+        <div class="col-2 col-sm-12">
+        </div>
+        <div class="col-2 col-sm-12">
+          <label class="form-label" for="f_name">First Name</label>
+        </div>
+        <div class="col-6 col-sm-12">
+          <input class="form-input" type="text" id="f_name" placeholder="John">
+        </div>
+        <div class="col-2 col-sm-12">
+        </div>
+      </div>
+      <div class="form-group">
+      <div class="col-2 col-sm-12">
+      </div>
+      <div class="col-2 col-sm-12">
+        <label class="form-label" for="l_name">Last Name</label>
+      </div>
+      <div class="col-6 col-sm-12">
+        <input class="form-input" type="text" id="l_name" placeholder="Doe">
+      </div>
+      <div class="col-2 col-sm-12">
+      </div>
+    </div>
+      <div class="form-group">
+        <div class="col-2 col-sm-12">
+        </div>
+        <div class="col-2 col-sm-12">
+          <label class="form-label">Birth Date</label>
+        </div>
+        <div class="col-6 col-sm-12">
+          <DatePicker v-model="date" :mode="'date'"
+                      :is-required="true"
+                      :max-date="new Date()"
+          />
+        </div>
+        <div class="col-2 col-sm-12">
+        </div>
+      </div>
+    </form>
+</template>
+
+<script>
+import { DatePicker } from 'v-calendar';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    DatePicker,
+  },
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+  watch: {
+    date() {
+      console.log(this.date);
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form-horizontal {
+  width: 50%;
+}
+</style>
